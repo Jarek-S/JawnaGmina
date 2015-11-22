a3037247e5d53858a5281393eed3209209922522
Jarek Spirydowicz <jareks@naszepolice.pl>
2015-11-22 20:02:18 +0100

    first basic import script - for .txt "line separated" file

--- konwerTxtJson.php
+++ konwerTxtJson.php
@@ -8,6 +8,19 @@
 <?php
     define('PLIK_WE', 'umowy.txt');
     define('PLIK_WY', 'dane.json');
+
+//checks array $set, adds $element if not found
+    function addToUniqueSet($set,$element) {
+        $flag = 0;
+        for($i=0; $i<count($set); $i++) {
+            if ($set[$i] == $element) 
+            $flag = $flag + 1;
+        };
+        if ($flag==0)
+        $set[count($set)] = $element;
+        return $set;
+    };
+
     $dane = file(PLIK_WE, FILE_IGNORE_NEW_LINES);
     if (((count($dane)-8)%6)==0) {
         for ($i=8, $j=0; $i<count($dane); $i=$i+6, $j++) {
@@ -20,9 +33,10 @@
                 "strona" => $dane[$i+5]
             );
         }
-    echo "Baza danych zawiera ".count($items)." pozycji.";
+//    echo "Tabela transakcji zawiera ".count($items)." rekordów.";
     
-    $header = array(
+    
+    $transactions = array(
         "zleca" => $dane[0],
         "wydzial" => $dane[1],
         "col_lp" => $dane[2],
@@ -31,9 +45,17 @@
         "col_kwota" => $dane[5],
         "col_okres" => $dane[6],
         "col_strona" => $dane[7],
-        "dane" => $items
+        //"dane" => $items
     );
-    echo json_encode($header, JSON_UNESCAPED_UNICODE);
+    //$test_array = array("zleca", "wydział", "Symbol umowy", "Gmina", "firma");
+    $strona_array = array();
+    foreach($items as $item) {
+        $test_string = $item['strona'];
+        $strona_array = addToUniqueSet($strona_array, $test_string);
+    }
+    echo "Tabela transakcji zawiera ".count($items)." rekordów przy ".count($strona_array)." kontrahentach.";
+    
+//    echo json_encode($transactions, JSON_UNESCAPED_UNICODE);
     }
     else echo "Nieprawidłowa liczba pozycji";
 ?>
